a051b61614b40810409bfc88967941c157416905
Ahmed <ahmed.max7586@gmail.com>
2019-09-05 23:01:51 -0400

    done secure logIn

--- src/auth/auth-service.js
+++ src/auth/auth-service.js
@@ -1,3 +1,7 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('../config')
+
 const AuthService = {
     getUserWithUserName(db, user_name) {
         return db('thingful_users')
@@ -6,6 +10,20 @@ const AuthService = {
             })
             .first()
     },
+    comparePasswords(password, hash) {
+        return bcrypt.compare(password, hash)
+    },
+      createJwt(subject, payload) {
+     return jwt.sign(payload, config.JWT_SECRET, {
+       subject,
+       algorithm: 'HS256',
+     })
+   },
+   verifyJwt(token) {
+       return jwt.verify(token, config.JWT_SECRET, {
+           algorithms:['HS256']
+       })
+   } ,
     parseBasicToken(token) {
         return Buffer
             .from(token, 'base64')
